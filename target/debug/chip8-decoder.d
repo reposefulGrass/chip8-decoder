9754135129913faeaa16837d5922e911d0d8c179
reposeful grass <reposefulGrass@gmail.com>
2022-11-07 23:46:42 -0600

    Remove unnecessary files, try 2

--- target/debug/chip8-decoder.d
+++
@@ -1,1 +0,0 @@
-/Users/wingocs1/Desktop/chip8-decoder/target/debug/chip8-decoder: /Users/wingocs1/Desktop/chip8-decoder/src/main.rs
--- target/debug/deps/chip8_decoder-54bb02baac06d5c0.d
+++
@@ -1,5 +0,0 @@
-/Users/wingocs1/Desktop/chip8-decoder/target/debug/deps/chip8_decoder-54bb02baac06d5c0: src/main.rs
-
-/Users/wingocs1/Desktop/chip8-decoder/target/debug/deps/chip8_decoder-54bb02baac06d5c0.d: src/main.rs
-
-src/main.rs:

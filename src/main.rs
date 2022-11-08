5d6798a2dcfc7774384ee325c1de069a155cb883
reposeful grass <reposefulGrass@gmail.com>
2022-11-07 23:44:40 -0600

    Add SYS instruction

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
 
+#[derive(Debug)]
+enum Instruction {
+	SYS(u16),
+	INVALID
+}
+
+impl Instruction {
+	fn decode (bytes: u16) -> Self {
+		match (bytes & 0xF000) >> 12 {
+			0 => {
+				Instruction::SYS(bytes & 0x0FFF)	
+			},	
+			_ => {
+				Instruction::INVALID
+			}
+		}
+	}
+}
+
 fn main() {
-    println!("Hello, world!");
+	let instr: u16 = 0x0001;
+	println!("{:?}", Instruction::decode(instr));
 }
---
+++ target/debug/chip8-decoder.d
@@ -0,0 +1,1 @@
+/Users/wingocs1/Desktop/chip8-decoder/target/debug/chip8-decoder: /Users/wingocs1/Desktop/chip8-decoder/src/main.rs
